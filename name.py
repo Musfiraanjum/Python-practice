256b130630d33b74c01b4107e6a0c4039835249d
Musfiraanjum <anjumamar1@gmail.com>
2025-03-24 17:05:21 +0530

    name file

---
+++ name.py
@@ -0,0 +1,220 @@
+# array = [2,6,4,86,98,81,38]
+# lar = -1
+# sed = -1
+# for i in array:
+#     if i >= lar: 
+#         lar = i
+# for i in array:
+#     if i > sed and i != lar:
+#         sed = i
+# print(sed)
+"juice shop"
+# amount = int(input("enter a amount::))
+# juice = 25
+# if juice == amount:
+#     print("take your juice 🍹")
+# elif juice>amount:
+#     print("this money is not safishiant ")
+# else:
+#     change = amount-juice
+#     print("this is your change",change)
+#     print("take your juice 🍹")
+"dress shop"
+# amount = int(input("enter a amount:"))
+# dress = 500
+# if dress == amount:
+#     print("this is your dress: 👗:") 
+# elif dress > amount:
+#     print("the amount is not safishiant")
+# elif dress > amount and dress == 1000:
+#     print("this is your two dresses :👗 👗")
+#     print("this is your gift:🎁")
+# else:
+#     change = amount - dress
+#     print("this is your change: ₹₹",change)
+#     print("this is your dress :👗")
+
+# elif dress == 1000:
+#     print("this is your two dresses :👗 👗")
+#     print("this is your gift:🎁")
+"food store"
+# choose = input(" a = apple🍏 , b = banana🍌 , C = chocolate🍫 enter A or B or C:" )
+# if choose == 'A':
+#     print("give me Apple:🍎 ")
+#     print("this is your apple :🍎")
+# elif choose ==' B':
+#     print("give me a banana:🍌")
+#     print("this is your banana:🍌")
+# elif choose =='C':
+#     print("give me a chocolate:🍫")
+#     print("this is your chocolate:🍫")
+# else:
+#     print("ok you take this  water:🍶")
+#     print("thank you ✋")
+"secson"
+# choise = input("s = summer 🌞, w = wintter⛅ , r = rain 🌊, choose any one :")
+# if choise == 's':
+#     print(" Oo this is summer,🌞 ")
+#     print("take cool water:🍸")
+# elif choise == 'w':
+#     print("Oo this is winter⛅ :")
+#     print("take your swetter 👕")
+# elif  choise == 'r':
+#     print("Oo this is rainy sesan :🌊")
+#     print("take your umbl ☔")
+# else:
+#     print("this is other sesan")
+"nationality"
+# nationality = input("I = India, P = Pakistan, E = England, L = London, A = America. Choose any one: ").lower()
+# if nationality == 'i':
+#     print("I am Indian.")
+#     religion = input("M = Muslim 🕌, H = Hindu 🏰, S = Sikh 🕍, C = Christian ⛪. Choose any one: ").lower()
+
+#     if religion == 'm':
+#         print("I am an Indian Muslim 🕌.")
+#     elif religion == 'h':
+#         print("I am an Indian Hindu 🏰.")
+#     elif religion == 's':
+#         print("I am an Indian Sikh 🕍.")
+#     elif religion == 'c':
+#         print("I am an Indian Christian ⛪.")
+#     else:
+#         print("Invalid choice.")
+
+# elif nationality == 'p':
+#     print("I am Pakistani.")
+# elif nationality == 'e':
+#     print("I am from England.")
+# elif nationality == 'l':
+#     print("I am from London.")
+# elif nationality == 'a':
+#     print("I am American.")
+# else:
+#     print("Invalid choice.")
+
+# "world trip "
+# print("🌍 Welcome to the Travel Recommendation System! 🌍")
+# mood = input("What type of trip do you want? \nA = Adventure 🏕️ \nR = Relaxing 🏖️ \nH = Historical 🏰 \nN = Nature 🌲 \nChoose (A/R/H/N): ").lower()
+# budget = input("What is your budget? \nL = Low 💸 \nM = Medium 💰 \nH = High 💎 \nChoose (L/M/H): ").lower()
+
+# if mood == 'a':  # Adventure
+#     if budget == 'l':
+#         print("🏕️ You should visit Rishikesh, India for budget-friendly adventure sports!")
+#     elif budget == 'm':
+#         print("🏔️ You should visit Nepal for an amazing Himalayan trekking experience!")
+#     elif budget == 'h':
+#         print("🏂 You should visit Switzerland for skiing and mountain adventures!")
+
+# elif mood == 'r':  # Relaxing
+#     if budget == 'l':
+#         print("🏖️ You should visit Goa, India for a relaxing beach vacation!")
+#     elif budget == 'm':
+#         print("🌊 You should visit Bali, Indonesia for a perfect tropical retreat!")
+#     elif budget == 'h':
+#         print("🌴 You should visit the Maldives for a luxurious island vacation!")
+
+# elif mood == 'h':  # Historical
+#     if budget == 'l':
+        # print("🏰 You should visit Jaipur, India to explore royal palaces and forts!")
+#     elif budget == 'm':
+#         print("🕌 You should visit Egypt to see the ancient pyramids!")
+#     elif budget == 'h':
+#         print("🏛️ You should visit Rome, Italy to witness historical wonders!")
+
+# elif mood == 'n':  # Nature
+#     if budget == 'l':
+#         print("🌲 You should visit Munnar, India to enjoy tea gardens and hills!")
+#     elif budget == 'm':
+#         print("🏔️ You should visit Canada for breathtaking national parks!")
+#     elif budget == 'h':
+#         print("🌿 You should visit New Zealand for an unforgettable nature experience!")
+# elif mood == 'f': #friend
+#     if budget == 'l':
+#         print("You should visit ooty, India to enjoy snoo  and hills!") 
+#     elif budget == 'm':
+#         print(" You should visit delhi agra ,to see the consterction of love!")
+#     elif budget == 'h':
+#           print(" You should visit HYDCITY ,to see film city an charminare and ect...!")
+# else:
+#     print("❌ Invalid choice. Please restart and select a valid option.")
+
+# print("✨ Have a great trip! ✨")
+
+"Seasonal World"
+# 🌍 Season Chooser - A Fun Experience 🎉
+# print("🌞⛅🌊 Welcome to the Seasonal World! 🌊⛅🌞")
+# print("------------------------------------------------")
+
+# choice = input("Choose your favorite season:\n"
+#                "🔹 s = Summer 🌞\n"
+#                "🔹 w = Winter ⛅\n"
+#                "🔹 r = Rainy 🌊\n"
+#                "🔹 (any other key = Unknown Season 🤔)\n"
+#                "👉 Enter your choice: ").lower()
+# print("\n------------------------------------------------")
+
+# if choice == 's':
+#     print("🔥 Oh wow! It's **SUMMER**! 🌞 Time for beaches and ice cream! 🍦")
+#     print("💡 Stay cool & hydrated! Have a refreshing drink! 🍸")
+
+# elif choice == 'w':
+#     print("❄️ Brrr... It's **WINTER**! ⛅ Time for cozy sweaters and warm coffee! ☕")
+#     print("🧣 Don't forget your sweater! 👕 Stay warm and enjoy!")
+
+# elif choice == 'r':
+#     print("🌧️ Oh no! It's **RAINY SEASON**! 🌊 Enjoy the sound of raindrops! 🎶")
+#     print("☔ Don't forget your umbrella! Stay dry and happy!")
+
+# else:
+#     print("🤔 Hmm... That doesn't seem like a season I know! 😅")
+#     print("🌍 Every season has its beauty, enjoy whatever comes your way!")
+
+# print("\n------------------------------------------------")
+# print("🌈 No matter the season, make it AWESOME! 💖🎉\n")
+
+
+# print("ente your name::")
+# name = input()
+# print(name,"welcome sma and good luck")
+# check=input("say 1.yes or 2.no")
+# if check == 'yes':
+#     print("thank you .can i start my ")
+# else:
+#     print("sorry")
+# from colorama import Fore, Style, init
+# init(autoreset=True)
+# # Getting input from the user (Corrected)
+# title = input(Fore.BLUE + "Enter the Title: ")
+# date = input(Fore.YELLOW + "Enter the Date (YYYY-MM-DD): ")
+# introduction = input(Fore.YELLOW + "Enter the Introduction: ")
+# experience = input(Fore.YELLOW + "Enter your Experience: ")
+# learnings = input(Fore.YELLOW + "Enter your Learnings: ")
+# conclusion = input(Fore.YELLOW + "Enter the Conclusion: ")
+
+# # Display the formatted report (Corrected and Improved)
+# print(Fore.CYAN + "*" * 50)
+# print(Fore.BLUE + "Title: " + title)  
+# print(Fore.BLUE + "Date: " + date)    
+# # print(Fore.CYAN + "*" * 50)
+# print(Fore.GREEN + "Introduction: " + introduction)  
+# print(Fore.WHITE + introduction)      # Corrected: printing the variable
+# print(Fore.MAGENTA + "Experience: " + experience)  
+# print(Fore.WHITE + experience)          
+# print(Fore.YELLOW + "*" * 50)         
+# print(Style.RESET_ALL)          
+
+
+# print("Choose your favorite criketer::-\n"
+#       "men plyaer\n"
+#       "wemen plyaer\n")
+# print("bowler\n"
+#       "batter\n")
+# name = input("enter a name ")
+# print("name of the plyer is",name)
+
+
+
+
+
+
+
